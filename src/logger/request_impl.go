16f0ae8a6c55aa6836a7e49536f8ce79617800ca
eswd0 <jili6190@colorado.edu>
2021-01-28 05:18:38 -0700

    created request implementation

---
+++ src/logger/request_impl.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+	import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
