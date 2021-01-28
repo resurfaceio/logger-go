2c2337f82e9c6853477a00288438d789e4750723
Kolin Newby <kolin.newby@gmail.com>
2021-01-27 21:13:52 -0700

    Create httpLoggerRules_test.go
    Created basic framework for http Logger Rules Tests.

---
+++ src/logger/httpLoggerRules_test.go
@@ -0,0 +1,108 @@
+import (
+	"testing"
+	
+)
+// Still need to import logger above
+
+
+// test override default rules
+
+func TestOverrideDefaultRules() {
+
+}
+
+// test uses allow http url rules
+
+func TestUsesAllowHttpUrlRules() {
+	var logger := BaseClientLogger{}
+}
+
+// test uses remove rules
+
+func TestUsesRemoveRules() {
+
+}
+
+// test uses remove if rules
+
+func TestUsesRemoveIfRules() {
+
+}
+
+// test uses remove if found rules
+
+func TestUsesRemoveIfFoundRules() {
+
+}
+
+// test uses remove unless rules
+
+func TestUsesRemoveUnlessRules() {
+
+}
+
+// test uses remove unless found rules
+
+func TestUsesRemoveUnlessFoundRules() {
+
+}
+
+// test uses replace rules
+
+func TestUsesReplaceRules() {
+
+}
+
+// test uses replace rules with complex expressions
+
+func TestUsesReplaceRulesWithComplexExpressions() {
+
+}
+
+// test uses sample rules
+
+func TestUsesSampleRules() {
+
+}
+
+// test uses skip compression rules
+
+func TestUsesSkipCompressionRules() {
+
+}
+
+// test uses skip submission rules
+
+func TestUsesSkipSubmissionRules() {
+
+}
+
+// test uses stop rules
+
+func TestUsesStopRules() {
+
+}
+
+// test uses stop if rules
+
+func TestUsesStopIfRules() {
+
+}
+
+// test uses stop if found rules
+
+func TestUsesStopIfFoundRules() {
+
+}
+
+// test uses stop unless rules
+
+func TestUsesStopUnlessRules() {
+
+}
+
+// test uses stop unless found rules
+
+func TestUsesStopUnlessFoundRules() {
+
+}
