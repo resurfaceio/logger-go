7c6f55721f57030eaff4cd21234ba3cece9a0c17
Timothy Lenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-01-27 21:41:23 -0700

    function headers added for most tests

---
+++ src/logger/HttpRule_test.go
@@ -0,0 +1,95 @@
+package logger
+
+import (
+	"testing"
+)
+
+func TestChangesDefaultRules(t *testing.T) {
+
+}
+
+func TestIncludesDebugRules(t *testing.T) {
+
+}
+
+func TestIncludesStandardRules(t *testing.T) {
+
+}
+
+func TestIncludesStrictRules(t *testing.T) {
+
+}
+
+func TestLoadRulesFromFile(t *testing.T) {
+
+}
+
+// func parseFail(line String) {
+
+// }
+
+// func parseOk(line String) {
+
+// }
+
+func TestPareseEmptyRules(t *testing.T) {
+
+}
+
+func TestParsesRulesWithBadVerbs(t *testing.T) {
+
+}
+
+func TestParsesRulesWithInvalidScopesTest(t *testing.T) {
+
+}
+
+func TestParsesAllowHttpRules(t *testing.T) {
+
+}
+
+func TestParsesCopySessionFieldRules(t *testing.T) {
+
+}
+
+func TestParsesRemoveRules(t *testing.T) {
+
+}
+
+func TestParsesRemoveIfRules(t *testing.T) {
+
+}
+
+func TestParsesRemoveIfFoundRules(t *testing.T) {
+
+}
+
+func TestParsesRemoveUnlessRules(t *testing.T) {
+
+}
+
+func TestParsesRemoveUnlessFoundRules(t *testing.T) {
+
+}
+
+func TestParsesReplaceRules(t *testing.T) {
+
+}
+
+func TestParsesSampleRules(t *testing.T) {
+
+}
+
+func TestParsesSkipCompressionRules(t *testing.T) {
+
+}
+
+func TestParsesSkipSubmissionRules(t *testing.T) {
+
+}
+
+func TestParsesStopRules(t *testing.T) {
+
+}
+
+//next test on line 682 of java logger HttpRulesTest.java
