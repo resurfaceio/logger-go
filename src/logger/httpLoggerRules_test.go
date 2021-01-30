c08223ca31543d578621e2d67750f40a3b3e9f5a
Kolin Newby <kolin.newby@gmail.com>
2021-01-29 20:56:19 -0700

    Update httpLoggerRules_test.go
    Cleaned up empty function frames

--- src/logger/httpLoggerRules_test.go
+++ src/logger/httpLoggerRules_test.go
@@ -1,9 +1,8 @@
 import (
 	"testing"
-	
 )
-// Still need to import logger above
 
+// Still need to import logger above
 
 // test override default rules
 
@@ -15,98 +14,37 @@ func TestOverrideDefaultRules() {
 
 func TestUsesAllowHttpUrlRules() {
 
-	logger := BaseClientLogger{}
-	url := "http://mysite.com"
-	rule := ""
+	logger := newLogger()
+	logger.
 
 }
 
 // test uses remove rules
 
-func TestUsesRemoveRules() {
-
-}
-
 // test uses remove if rules
-
-func TestUsesRemoveIfRules() {
-
-}
 
 // test uses remove if found rules
 
-func TestUsesRemoveIfFoundRules() {
-
-}
-
 // test uses remove unless rules
-
-func TestUsesRemoveUnlessRules() {
-
-}
 
 // test uses remove unless found rules
 
-func TestUsesRemoveUnlessFoundRules() {
-
-}
-
 // test uses replace rules
-
-func TestUsesReplaceRules() {
-
-}
 
 // test uses replace rules with complex expressions
 
-func TestUsesReplaceRulesWithComplexExpressions() {
-
-}
-
 // test uses sample rules
-
-func TestUsesSampleRules() {
-
-}
 
 // test uses skip compression rules
 
-func TestUsesSkipCompressionRules() {
-
-}
-
 // test uses skip submission rules
-
-func TestUsesSkipSubmissionRules() {
-
-}
 
 // test uses stop rules
 
-func TestUsesStopRules() {
-
-}
-
 // test uses stop if rules
-
-func TestUsesStopIfRules() {
-
-}
 
 // test uses stop if found rules
 
-func TestUsesStopIfFoundRules() {
-
-}
-
 // test uses stop unless rules
 
-func TestUsesStopUnlessRules() {
-
-}
-
 // test uses stop unless found rules
-
-func TestUsesStopUnlessFoundRules() {
-
-}
