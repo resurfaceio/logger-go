b45e138574c197014ceac5791ba7163bfa245894
Timothy Lenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-02-02 18:15:23 -0700

    WIP

--- src/logger/HttpMessage.go
+++ src/logger/HttpMessage.go
@@ -1,7 +1,14 @@
 package logger
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+/*
+* Builds list of key/value pairs for HTTP request and response.
+ */
 func buildNetHttpClientMessage(logger *NetHttpClientLogger, resp *http.Response) {
 	request := resp.Request
 
@@ -16,27 +23,43 @@ func buildNetHttpClientMessage(logger *NetHttpClientLogger, resp *http.Response)
 		message = append(message, []string{"request_method", method})
 	}
 
-	formattedURL := formatURL(request)
-	if formattedURL != "" {
-		message = append(message, []string{"request_url", formattedURL})
+	message = append(message, []string{"request_url", request.URL.RequestURI()})
+	message = append(message, []string{"response_code", fmt.Sprint(resp.StatusCode)})
+
+	appendRequestHeaders(message, request)
+	appendRequestParams(message, request)
+	appendResponseHeaders(message, resp)
+
+	reqBodyBytes, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		message = append(message, []string{"request_body", string(reqBodyBytes)})
 	}
 
-	message = append(message, []string{"response_code", string(resp.StatusCode)})
+	respBodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		message = append(message, []string{"response_body", string(respBodyBytes)})
+	}
 
 }
 
-func appendResponseHeaders(resp *http.Response) {
+/*
+* Adds response headers to message.
+ */
+func appendResponseHeaders(message [][]string, resp *http.Response) {
 
 }
 
-func appendRequestParams(req *http.Request) string {
+/*
+* Adds request params to message.
+ */
+func appendRequestParams(message [][]string, req *http.Request) {
 
 }
 
-func appendRequestHeaders(req *http.Request) string {
-
-}
-
-func formatURL(req *http.Request) string {
+/*
+* Adds request headers to message.
+ */
+func appendRequestHeaders(message [][]string, req *http.Request) {
 
 }
