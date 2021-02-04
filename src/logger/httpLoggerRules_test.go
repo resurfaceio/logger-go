50aa414b6e182394c8ad8cbb19c4cfbf3d92e839
Kolin Newby <kolin.newby@gmail.com>
2021-02-04 13:26:06 -0700

    Update httpLoggerRules_test.go
    finished up TestUsesAllowHttpUrlRules

--- src/logger/httpLoggerRules_test.go
+++ src/logger/httpLoggerRules_test.go
@@ -18,20 +18,28 @@ func TestOverrideDefaultRules() {
 
 func TestUsesAllowHttpUrlRules(t *testing.T) {
 	// requires url, rules, and enableable to be in logger struct !!!
-	testLogger1 := baseClientLogger{url: "http://mysote.com"}
-	assert.Equal(t, testLogger1.enableable, false, "Logger enableable flag should be set to false")
+	testLogger := newLogger()
+	testLogger.url = "http://mysite.com"
+	assert.Equal(t, false, testLogger.enableable, "Logger enableable flag should be set to false")
 
-	testLogger2 := baseClientLogger{url: "http://mysote.com", rules: ""}
-	assert.Equal(t, testLogger2.enableable, false, "Logger enableable flag should be set to false")
+	testLogger = newLogger()
+	testLogger.url = "http://mysite.com"
+	testLogger.rules = ""
+	assert.Equal(t, false, testLogger.enableable, "Logger enableable flag should be set to false")
 
-	testLogger3 := baseClientLogger{url: "https://mysote.com"}
-	assert.Equal(t, testLogger3.enableable, true, "Logger enableable flag should be set to true")
+	testLogger = newLogger()
+	testLogger.url = "https://mysite.com"
+	assert.Equal(t, true, testLogger.enableable, "Logger enableable flag should be set to true")
 
-	testLogger4 := baseClientLogger{url: "http://mysote.com", rules: "allow_http_url"}
-	assert.Equal(t, testLogger4.enableable, true, "Logger enableable flag should be set to true")
+	testLogger = newLogger()
+	testLogger.url = "http://mysite.com"
+	testLogger.rules = "allow_http_url"
+	assert.Equal(t, true, testLogger.enableable, "Logger enableable flag should be set to true")
 
-	testLogger5 := baseClientLogger{url: "http://mysote.com", rules: "allow_http_url\nallow_http_url"}
-	assert.Equal(t, testLogger5.enableable, true, "Logger enableable flag should be set to true")
+	testLogger = newLogger()
+	testLogger.url = "http://mysite.com"
+	testLogger.rules = "allow_http_url\nallow_http_url"
+	assert.Equal(t, true, testLogger.enableable, "Logger enableable flag should be set to true")
 }
 
 // test uses remove rules
