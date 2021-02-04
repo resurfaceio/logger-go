62cb395dbae2f409d92f2c0cd9ca6a2c5ddb0c11
Kolin Newby <kolin.newby@gmail.com>
2021-02-04 14:23:00 -0700

    Update httpLoggerRules_test.go
    Added code for TestOverrideDefaultRules function

--- src/logger/httpLoggerRules_test.go
+++ src/logger/httpLoggerRules_test.go
@@ -6,40 +6,91 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Still need to import logger above
+// Still need to import
 
 // test override default rules
 
-func TestOverrideDefaultRules() {
+func TestOverrideDefaultRules(t *testing.T) {
+	assert.Equal(t, httpRules.strictRules, httpRules.defaultRules, "HTTP default rules are not strict rules")
 
+	logger := newLogger()
+	logger.url = "https://mysite.com"
+	assert.Equal(t, httpRules.strictRules, logger.rules.text, "logger rules are not set to default rules")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = "# 123"
+	assert.Equal(t, "# 123", logger.rules.text, "logger default rules not overriden")
+
+	httpRules.setDefaultRules("")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	assert.Equal(t, "", logger.rules.text, "logger default rules were not applied")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = "   "
+	assert.Equal(t, "", logger.rules.text, "logger default rules not overriden or blank space not ignored")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = " sample 42"
+	assert.Equal(t, " sample 42", logger.rules.text, "logger default rules not overriden")
+
+	httpRules.setDefaultRules("skip_compression")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	assert.Equal(t, "skip_compression", logger.rules.text, "logger default rules not applied")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = "include default\nskip_submission\n"
+	assert.Equal(t, "include default\nskip_submission\n", logger.rules.text, ":logger default rules not overriden")
+
+	httpRules.setDefaultRules("sample 42\n")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	assert.Equal(t, "sample 42\n", logger.rules.text, "logger default rules not applied")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = "   "
+	assert.Equal(t, "sample 42\n", logger.rules.text, "white space not ignored")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = "include default\nskip_submission\n"
+	assert.Equal(t, "sample 42\n\nskip_submission\n", logger.rules.text, "logger rules not applied correctly")
+
+	httpRules.setDefaultRules("inlude debug")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	logger.rules = httpRules.strictRules
+	assert.Equal(t, httpRules.strictRules, logger.rules.text, "logger default rules not overriden")
+
+	httpRules.setDefaultRules(httpRules.strictRules)
 }
 
 // test uses allow http url rules
 
 func TestUsesAllowHttpUrlRules(t *testing.T) {
 	// requires url, rules, and enableable to be in logger struct !!!
-	testLogger := newLogger()
-	testLogger.url = "http://mysite.com"
-	assert.Equal(t, false, testLogger.enableable, "Logger enableable flag should be set to false")
+	logger := newLogger()
+	logger.url = "http://mysite.com"
+	assert.Equal(t, false, logger.enableable, "Logger enableable flag should be set to false")
 
-	testLogger = newLogger()
-	testLogger.url = "http://mysite.com"
-	testLogger.rules = ""
-	assert.Equal(t, false, testLogger.enableable, "Logger enableable flag should be set to false")
+	logger = newLogger()
+	logger.url = "http://mysite.com"
+	logger.rules = ""
+	assert.Equal(t, false, logger.enableable, "Logger enableable flag should be set to false")
 
-	testLogger = newLogger()
-	testLogger.url = "https://mysite.com"
-	assert.Equal(t, true, testLogger.enableable, "Logger enableable flag should be set to true")
+	logger = newLogger()
+	logger.url = "https://mysite.com"
+	assert.Equal(t, true, logger.enableable, "Logger enableable flag should be set to true")
 
-	testLogger = newLogger()
-	testLogger.url = "http://mysite.com"
-	testLogger.rules = "allow_http_url"
-	assert.Equal(t, true, testLogger.enableable, "Logger enableable flag should be set to true")
+	logger = newLogger()
+	logger.url = "http://mysite.com"
+	logger.rules = "allow_http_url"
+	assert.Equal(t, true, logger.enableable, "Logger enableable flag should be set to true")
 
-	testLogger = newLogger()
-	testLogger.url = "http://mysite.com"
-	testLogger.rules = "allow_http_url\nallow_http_url"
-	assert.Equal(t, true, testLogger.enableable, "Logger enableable flag should be set to true")
+	logger = newLogger()
+	logger.url = "http://mysite.com"
+	logger.rules = "allow_http_url\nallow_http_url"
+	assert.Equal(t, true, logger.enableable, "Logger enableable flag should be set to true")
 }
 
 // test uses remove rules
