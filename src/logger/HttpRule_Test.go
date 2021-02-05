090f42300ab8f25b000b2404dad44f9c1610e1d1
Timothy Lenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-02-05 15:58:10 -0700

    WIP

--- src/logger/HttpRule_Test.go
+++ src/logger/HttpRule_Test.go
@@ -6,47 +6,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestChangesDefaultRules(t *testing.T) {
+func TestIncludeDebugRules(t *testing.T) {
+	rules := newHttpRules()
+	assert.Equal(t, 2, rules.Size)
+	assert.True(t, rules.AllowHttpUrl)
+	assert.Equal(t, 1, len(rules.CopySessionField))
+
+	rules = newHttpRules("include debug\n")
+	assert.Equal(t, 2, rules.Size)
+	rules = newHttpRules("include debug\nsample 50")
+	assert.Equal(t, 3, rules.Size)
+	assert.Equal(t, 1, len(rules.Sample))
+
+	rules = newHttpRules(" include debug\ninclude debug")
+	assert.Equal(t, 4, rules.Size)
+	rules = newHttpRules("include debug\nsample 50\ninclude debug")
+	assert.Equal(t, 5, rules.Size)
+
 	httpRules := GetHttpRules()
+	assert.Equal(t, httpRules.StrictRules(), httpRules.DefaultRules())
+	httpRules.SetDefaultRules("include debug")
+	rules = newHttpRules("")
 	for {
-		if !assert.Equal(t, newHttpRules("").StrictRules(), httpRules.DefaultRules()) {
+		if !assert.Equal(t, 2, rules.Size) {
 			break
 		}
 
-		httpRules.SetDefaultRules("")
-		if !assert.Equal(t, "", httpRules.DefaultRules()) {
-			break
-		}
-		if !assert.Equal(t, 0, newHttpRules(httpRules.DefaultRules()).size) {
+		if !assert.True(t, rules.AllowHttpUrl) {
 			break
 		}
 
-		httpRules.SetDefaultRules(" include default")
-		if !assert.Equal(t, "", httpRules.DefaultRules()) {
+		if !assert.Equal(t, 1, len(rules.CopySessionField)) {
 			break
 		}
 
-		httpRules.SetDefaultRules("include default\n")
-		if !assert.Equal(t, "", httpRules.DefaultRules()) {
-			break
-		}
-
-		httpRules.SetDefaultRules("include default\ninclude default\n")
-		if !assert.Equal(t, 0, newHttpRules(httpRules.DefaultRules()).size) {
-			break
-		}
-
-		httpRules.SetDefaultRules("include default\ninclude default\nsample 42")
-		rules := newHttpRules(httpRules.DefaultRules())
-		if !assert.Equal(t, 1, rules.size) {
-			break
-		}
-		if !assert.Equal(t, 1, len(rules.sample)) {
-			break
-		}
 		break
 	}
 
 	httpRules.SetDefaultRules(httpRules.StrictRules())
-
 }
