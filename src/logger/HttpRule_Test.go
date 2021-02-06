622f550b17a8c972179dd197755e018933f966e6
Timothy Lenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-02-05 18:49:10 -0700

    copied current code from test branch because merge was being weird (or I did this wrong)

--- src/logger/HttpRule_Test.go
+++ src/logger/HttpRule_Test.go
@@ -6,6 +6,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestChangesDefaultRules(t *testing.T) {
+	httpRules := GetHttpRules()
+	for {
+		if !assert.Equal(t, newHttpRules("").StrictRules(), httpRules.DefaultRules()) {
+			break
+		}
+
+		httpRules.SetDefaultRules("")
+		if !assert.Equal(t, "", httpRules.DefaultRules()) {
+			break
+		}
+		if !assert.Equal(t, 0, newHttpRules(httpRules.DefaultRules()).Size) {
+			break
+		}
+
+		httpRules.SetDefaultRules(" include default")
+		if !assert.Equal(t, "", httpRules.DefaultRules()) {
+			break
+		}
+
+		httpRules.SetDefaultRules("include default\n")
+		if !assert.Equal(t, "", httpRules.DefaultRules()) {
+			break
+		}
+
+		httpRules.SetDefaultRules("include default\ninclude default\n")
+		if !assert.Equal(t, 0, newHttpRules(httpRules.DefaultRules()).Size) {
+			break
+		}
+
+		httpRules.SetDefaultRules("include default\ninclude default\nsample 42")
+		rules := newHttpRules(httpRules.DefaultRules())
+		if !assert.Equal(t, 1, rules.Size) {
+			break
+		}
+		if !assert.Equal(t, 1, len(rules.Sample)) {
+			break
+		}
+		break
+	}
+
+	httpRules.SetDefaultRules(httpRules.StrictRules())
+
+}
+
 func TestIncludeDebugRules(t *testing.T) {
 	rules := newHttpRules("include debug")
 	assert.Equal(t, 2, rules.Size)
