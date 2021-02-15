3c52a308068994a45d62c511706902f085d32ab6
ChandlerGarthwaite <chandler.garthwaite@gmail.com>
2021-02-15 12:13:46 -0700

    Adding initial functions

--- src/logger/NetHttpClientLogger_test.go
+++ src/logger/NetHttpClientLogger_test.go
@@ -8,6 +8,30 @@ import (
 
 //Testing NET HTTP Logger
 
+func TestLogsHtml(t *testing.T) {
+
+	queue := []string{}
+	filter := newLogger(queue, "includes standard")
+	helper := NewTestHelper()
+	filter.init(nil)
+	filter.doFilter(helper.mockRequest(), helper.mockResponse(), helper.mockJsonApp())
+	assert.Equal(t, 1, len(queue))
+	msg := queue[0]
+	assert.True(t, parseable(msg))
+	assert.Contains(t, msg, "[\"request_method\",\"GET\"]")
+	assert.Contains(t, msg, "[\"request_url\",\"" + helper.mockURL + "\"]")
+	assert.Contains(t, msg, "[\"response_body\",\"" + helper.mockHTML + "\"]")
+	assert.Contains(t, msg, "[\"response_code\",\"200\"]")
+	assert.Contains(t, msg, "[\"response_header:a\",\"Z\"]")
+	assert.Contains(t, msg, "[\"response_header:content-type\",\"text/html\"]")
+	assert.Contains(t, msg, "[\"now\",\"")
+	assert.Contains(t, msg, "[\"interval\",\"")
+	assert.NotContains(t, msg, "request_body")
+	assert.NotContains(t, msg, "request_header")
+	assert.NotContains(t, msg, "request_param")
+
+}
+
 func TestlogJson(t *testing.T) {
 	helper := NewTestHelper()
 	queue := []string{}
