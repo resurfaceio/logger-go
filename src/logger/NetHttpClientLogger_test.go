350a425c7639c57d26939968bf9fb8108c144e42
ChandlerGarthwaite <chandler.garthwaite@gmail.com>
2021-02-15 12:24:05 -0700

    Adding json log tests

--- src/logger/NetHttpClientLogger_test.go
+++ src/logger/NetHttpClientLogger_test.go
@@ -17,7 +17,7 @@ func TestLogsHtml(t *testing.T) {
 	filter.doFilter(helper.mockRequest(), helper.mockResponse(), helper.mockJsonApp())
 	assert.Equal(t, 1, len(queue))
 	msg := queue[0]
-	assert.True(t, parseable(msg))
+	assert.True(t, parsable(msg))
 	assert.Contains(t, msg, "[\"request_method\",\"GET\"]")
 	assert.Contains(t, msg, "[\"request_url\",\"" + helper.mockURL + "\"]")
 	assert.Contains(t, msg, "[\"response_body\",\"" + helper.mockHTML + "\"]")
@@ -29,7 +29,6 @@ func TestLogsHtml(t *testing.T) {
 	assert.NotContains(t, msg, "request_body")
 	assert.NotContains(t, msg, "request_header")
 	assert.NotContains(t, msg, "request_param")
-
 }
 
 func TestlogJson(t *testing.T) {
@@ -42,7 +41,7 @@ func TestlogJson(t *testing.T) {
 	assert.Equal(t, 1, len(queue))
 	msg := queue[0]
 	//must implement parsable function
-	//assert.True(t,parsable(msg))
+	assert.True(t,parsable(msg))
 	assert.Contains(t, msg, "[\"request_method\",\"GET\"]")
 	assert.Contains(t, msg, "[\"response_body\",\""+helper.mockJSONescaped+"\"]")
 	assert.Contains(t, msg, "[\"response_code\",\"200\"]")
@@ -50,4 +49,22 @@ func TestlogJson(t *testing.T) {
 	assert.NotContains(t, msg, "request_body")
 	assert.NotContains(t, msg, "request_header")
 	assert.NotContains(t, msg, "request_param")
+}
+
+func TestLogsJsonPost(t *testing.T){
+	queue := []string{}
+	filter := newLogger(queue, "includes standard")
+	helper := NewTestHelper()
+	filter.init(nil)
+	filter.doFilter(helper.mockRequestWithJson(), helper.mockResponse(), helper.mockJsonApp())
+	assert.Equal(t, 1, len(queue))
+	msg := queue[0]
+	assert.True(t, parsable(msg))
+	assert.Contains(t, msg, "[\"request_header:content-type\",\"Application/JSON\"]")
+	assert.Contains(t, msg, "[\"request_method\",\"POST\"]")
+	assert.Contains(t, msg, "[\"request_param:message\",\"" + helper.mockJSONescaped + "\"]")
+	assert.Contains(t, msg, "[\"request_url\",\"" + helper.mockURL + "?" + helper.mockQueryString + "\"]")
+	assert.Contains(t, msg, "[\"response_body\",\"" + helper.mockJSONescaped + "\"]")
+	assert.Contains(t, msg, "[\"response_code\",\"200\"]")
+	assert.Contains(t, msg, "[\"response_header:content-type\",\"application/json; charset=utf-8\"]")
 }
