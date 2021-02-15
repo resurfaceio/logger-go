cae2f4887a6d7641ac6448a062d80252caabedbf
ChandlerGarthwaite <chandler.garthwaite@gmail.com>
2021-02-15 11:22:15 -0700

    Reset branch, moving contents to new branch

--- src/logger/NetHttpClientLogger_test.go
+++ src/logger/NetHttpClientLogger_test.go
@@ -5,60 +5,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNetHttpClientLogger(t *testing.T) {
+//Testing NET HTTP Logger
+
+func TestCreateInstance(t *testing.T) {
 
 	//Creating a single instance
 	logger := newLogger()
-	testHelper := NewTestHelper()
-
-	assert.NotNil(t, logger)
-	assert.Equal(t, netHttpClientLogger.AGENT, logger.AGENT)
-	assert.False(t, logger.isEnableable)
-	assert.False(t, logger.LOGFLAG)
-	assert.Nil(t, logger.GetQueue)
-	assert.Nil(t, logger.GetURL)
-
-	//Creating multiple instances
-	url1 := "https://resurface.io"
-	url2 := "https://whatever.com"
-
-	logger1 := newLogger(url1)
-	logger2 := newLogger(url2)
-	logger3 := newLogger(testHelper.demoURL)
-
-	//Logger 1
-	assert.NotNil(t, logger1)
-	assert.Equal(t, netHttpClientLogger.AGENT, logger1.AGENT)
-	assert.False(t, logger1.isEnableable)
-	assert.False(t, logger1.LOGFLAG)
-	assert.Nil(t, logger1.GetQueue)
-	assert.Equal(t, url1, logger1.GetURL)
-
-	//Logger 2
-	assert.NotNil(t, logger2)
-	assert.Equal(t, netHttpClientLogger.AGENT, logger2.AGENT)
-	assert.False(t, logger2.isEnableable)
-	assert.False(t, logger2.LOGFLAG)
-	assert.Nil(t, logger2.GetQueue)
-	assert.Equal(t, url2, logger2.GetURL)
-
-	//Logger 3
-	assert.NotNil(t, logger3)
-	assert.Equal(t, netHttpClientLogger.AGENT, logger3.AGENT)
-	assert.False(t, logger3.isEnableable)
-	assert.False(t, logger3.LOGFLAG)
-	assert.Nil(t, logger3.GetQueue)
-	assert.Nil(t, logger3.GetURL)
-	assert.Equal(t, testHelper.demoURL, logger3.GetURL)
-
-
-	//Has Valid Agent Test
-	agent := logger.AGENT
-	assert.Greater(t, len(agent),0)
-	assert.Equal(t, ".go",agent[len(agent)-3:])
-	assert.False(t, assert.Contains(t, agent, "\\" ))
-	assert.False(t, assert.Contains(t, agent, "\"" ))
-	assert.False(t, assert.Contains(t, agent, "'" ))
-	assert.Equal(t, agent, newLogger().AGENT)
 
 }
+
