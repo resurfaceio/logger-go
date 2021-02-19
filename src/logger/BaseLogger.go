45a06a089e708bd84eee6b07660d4398c325fde5
KyleNeubarth <kyle@neubarth.org>
2021-02-18 19:04:05 -0700

    completed and organized main constructor

--- src/logger/BaseLogger.go
+++ src/logger/BaseLogger.go
@@ -41,34 +41,42 @@ func NewBaseLoggerAgentQueueEnabled(_agent string, _queue []string, _enabled boo
 
 //main constructor
 func NewBaseLogger(_agent string, _url string, _enabled bool, _queue []string) *BaseLogger {
-	baseLogger := &BaseLogger{}
-	baseLogger.agent = _agent
-	baseLogger.host = "please implement host_lookup()"
-	baseLogger.version = "please implement version_lookup()"
-	baseLogger.queue = _queue
 
-	//set options in priority order
-	baseLogger.enabled = _enabled
 	//I believe comparing with an empty string should be the same as comparing with nil
 	if _url == "" {
-		baseLogger.url = "urlByDefault, in the java version this is defined in UsageLoggers"
-		if baseLogger.url == "" {
-			baseLogger.enabled = false
-		} else {
-			baseLogger.url = _url
+		_url = UsageLoggers.urlByDefault()
+		if _url == "" {
+			_enabled = false
 		}
 	}
 
+	var _urlParsed *url.URL
+	var parsingError error
 	//validate url when present
-	if baseLogger.url != "" {
+	if _url != "" {
 		//I'm replacing the try catch statement with the error return
 		//but I'll implement that later, this line is a dummy
-		baseLogger.urlParsed, _ = url.Parse(baseLogger.url)
+		_urlParsed, parsingError = url.Parse(_url)
+		if parsingError != nil {
+			_url = ""
+			_urlParsed = nil
+			_enabled = false
+		}
 		//the rest is throwing errors
 	}
-	baseLogger.enableable = (baseLogger.url != "")
+	_enableable := (baseLogger.url != "")
 
-	return baseLogger
+	constructedBaseLogger := &BaseLogger{
+		agent:      _agent,
+		enableable: _enableable,
+		enabled:    _enabled,
+		host:       hostLookup(),
+		queue:      _queue,
+		url:        _url,
+		urlParsed:  _urlParsed,
+		version:    versionLookup(),
+	}
+	return constructedBaseLogger
 }
 
 //these functions could be redundant, basically setters
@@ -81,26 +89,27 @@ func (obj BaseLogger) Disable() {
 }
 
 /**
- * Returns host identifier for this logger.
- */
-func (obj BaseLogger) hostLookup() string {
-	dyno := "this is what System.getenv(dyno) will return"
-	if dyno == "" {
-		return dyno
-	}
-	//TODO: implement try/catch style error return
-	//Implement whatever this host code is
-	return "InetAddress.getLocalHost().getHostName()"
-}
-
-/**
  * Submits JSON message to intended destination.
  */
 func (obj BaseLogger) Submit(msg string) {
 	//woah congrats you submitted the message
 	//TODO: implement submit func
 }
-func (obj BaseLogger) versionLookup() string { return "0.0.0.wehaventstartedityet" }
+
+/**
+ * Returns host identifier.
+ * These are utility functions that can be static if this wasn't Go
+ */
+func hostLookup() string {
+	dyno := "this is what System.getenv(dyno) will return"
+	if dyno == "" {
+		return dyno
+	}
+	//TODO: implement try/catch style error return
+	//Implement whatever this host code is
+	return InetAddress.getLocalHost().getHostName()
+}
+func versionLookup() string { return "0.0.0.wehaventstartedityet" }
 
 func (obj BaseLogger) Agent() string         { return obj.agent }
 func (obj BaseLogger) Enableable() bool      { return obj.enableable }
@@ -115,8 +124,10 @@ func (obj BaseLogger) Url() string           { return obj.url }
 func (obj BaseLogger) UrlParsed() *url.URL   { return obj.urlParsed }
 func (obj BaseLogger) Version() string       { return obj.version }
 
-func (obj BaseLogger) SetSkipCompression(_skipCompression bool) {obj.skipCompression = _skipCompression}
-func (obj BaseLogger) SetSkipSubmission(_skipSubmission bool) {obj.skipSubmission = _skipSubmission}
+func (obj BaseLogger) SetSkipCompression(_skipCompression bool) {
+	obj.skipCompression = _skipCompression
+}
+func (obj BaseLogger) SetSkipSubmission(_skipSubmission bool) { obj.skipSubmission = _skipSubmission }
 
 type BaseLogger struct {
 	agent      string
