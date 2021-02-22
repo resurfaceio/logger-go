0933e908012d3d6865c1f38e444dd320702966b4
ChandlerGarthwaite <chandler.garthwaite@gmail.com>
2021-02-22 12:58:07 -0700

    Refine function names

--- src/logger/HttpClientLogger_test.go
+++ src/logger/HttpClientLogger_test.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,14 +11,14 @@ import (
 func TestCreateInstance(t *testing.T) {
 
 	//Creating a single instance
-	logger := newLogger()
-
-	assert.NotNil(t, logger)
-	assert.Equal(t, HttpLogger.AGENT, logger.AGENT)
-	assert.False(t, logger.isEnableable)
-	assert.False(t, logger.LOGFLAG)
-	assert.Nil(t, logger.GetQueue)
-	assert.Nil(t, logger.GetURL)
+	httpLogger := NewHttpLogger()
+	helper := GetTestHelper()
+	assert.NotNil(t, httpLogger)
+	assert.Equal(t, helper.mockAgent, httpLogger.Agent())
+	assert.False(t, httpLogger.Enablable())
+	assert.False(t, httpLogger.Enabled())
+	assert.Nil(t, httpLogger.Queue())
+	assert.Nil(t, httpLogger.Url())
 
 }
 
@@ -25,49 +26,50 @@ func TestCreateMultipleInstances(t *testing.T) {
 	//Creating multiple instances
 	url1 := "https://resurface.io"
 	url2 := "https://whatever.com"
-	testHelper := NewTestHelper()
+	helper := GetTestHelper()
 
-	logger1 := newLogger(url1)
-	logger2 := newLogger(url2)
-	logger3 := newLogger(Helper.getDemoUrl())
+	logger1 := NewHttpLogger(url1)
+	logger2 := NewHttpLogger(url2)
+	logger3 := NewHttpLogger(helper.demoURL)
 
 	//Logger 1
 	assert.NotNil(t, logger1)
-	assert.Equal(t, HttpLogger.AGENT, logger1.AGENT)
-	assert.False(t, logger1.isEnableable)
-	assert.False(t, logger1.LOGFLAG)
-	assert.Nil(t, logger1.GetQueue())
-	assert.Equal(t, url1, logger1.GetUrl())
+	assert.Equal(t, helper.mockAgent, logger1.AGENT())
+	assert.False(t, logger1.Enablable())
+	assert.False(t, logger1.Enabled())
+	assert.Nil(t, logger1.Queue())
+	assert.Equal(t, url1, logger1.Url())
 
 	//Logger 2
 	assert.NotNil(t, logger2)
-	assert.Equal(t, HttpLogger.AGENT, logger2.GetAgent())
-	assert.False(t, logger2.isEnableable)
-	assert.False(t, logger2.LOGFLAG)
-	assert.Nil(t, logger2.GetQueue())
-	assert.Equal(t, url2, logger2.GetUrl())
+	assert.Equal(t, helper.mockAgent, logger2.Agent())
+	assert.False(t, logger2.Enablable())
+	assert.False(t, logger2.Enabled())
+	assert.Nil(t, logger2.Queue())
+	assert.Equal(t, url2, logger2.Url())
 
 	//Logger 3
 	assert.NotNil(t, logger3)
-	assert.Equal(t, HttpLogger.AGENT, logger3.GetAgent())
-	assert.False(t, logger3.isEnableable)
-	assert.False(t, logger3.LOGFLAG)
-	assert.Nil(t, logger3.GetQueue())
-	assert.Nil(t, logger3.GetUrl())
-	assert.Equal(t, testHelper.GetDemoUrl(), logger3.GetUrl())
+	assert.Equal(t, helper.mockAgent, logger3.Agent())
+	assert.False(t, logger3.Enablable())
+	assert.False(t, logger3.Enabled())
+	assert.Nil(t, logger3.Queue())
+	assert.Nil(t, logger3.Url())
+	assert.Equal(t, helper.demoURL, logger3.Url())
 
 }
 
-func TestHasValidAgent(t *testing.T){
+func TestHasValidAgent(t *testing.T) {
 	//Has Valid Agent Test
-	agent := logger.AGENT
-	logger1 := newLogger()
-	assert.Greater(t, len(agent),0)
-	assert.Equal(t, ".go",agent[len(agent)-3:])
-	assert.NotContains(t, agent, "\\" )
-	assert.NotContains(t, agent, "\"" )
-	assert.NotContains(t, agent, "'" )
-	assert.Equal(t, agent, logger1.getAgent())
+	helper := GetTestHelper()
+	logger1 := NewHttpLogger()
+
+	agent := helper.mockAgent
+	assert.Greater(t, len(agent), 0)
+	assert.Equal(t, ".go", agent[len(agent)-3:])
+	assert.NotContains(t, agent, "\\")
+	assert.NotContains(t, agent, "\"")
+	assert.NotContains(t, agent, "'")
+	assert.Equal(t, agent, logger1.Agent())
 
 }
-
