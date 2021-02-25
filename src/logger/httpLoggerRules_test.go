15234fc5cdf985ffeb2a64d24cd16619a3fad1b5
Kolin Newby <kolin.newby@gmail.com>
2021-02-25 11:52:43 -0700

    Update httpLoggerRules_test.go

--- src/logger/httpLoggerRules_test.go
+++ src/logger/httpLoggerRules_test.go
@@ -459,6 +459,50 @@ func TestUsesRemoveUnlessRules(t *testing.T) {
 
 // test uses remove unless found rules
 
+func TestUsesRemoveUnlessFoundRules(t *testing.T) {
+    helper := GetTestHelper()
+
+    request := helper.MockRequestWithJson2()
+    mockResponse := helper.MockResponseWithHtml()
+
+    request.Body = ioutil.NopCloser(bytes.NewBufferString(helper.mockJson))
+    mockResponse.Body = ioutil.NopCloser(bytes.NewBufferString(helper.mockHtml))
+    mockResponse.Request = request
+
+    queue := make([]string, 0)
+    logger := NewHttpLoggerQueueRules(queue, "!response_header:blahblahblah! remove_unless_found !.*!")
+    httpMessage.sendNetHttpRequestResponseMessage(logger, mockResponse, 0, 0)
+    assert.Equal(t, 1, len(queue), "queue length is not 1")
+
+    queue = make([]string, 0)
+    logger = NewHttpLoggerQueueRules(queue, "!.*! remove_unless_found !blahblahblah!")
+    httpMessage.sendNetHttpRequestResponseMessage(logger, mockResponse, 0, 0)
+    assert.Equal(t, 0, len(queue), "queue is not empty")
+
+    queue = make([]string, 0)
+    logger = NewHttpLoggerQueueRules(queue, "!request_body! remove_unless_found !blahblahblah!")
+    httpMessage.sendNetHttpRequestResponseMessage(logger, mockResponse, 0, 0)
+    assert.Equal(t, 1, len(queue), "queue length is not 1")
+    assert.Equal(t, false, strings.Contains(queue[0], "[\"request_body\","), "request_body not removed")
+    assert.Equal(t, true, strings.Contains(queue[0], "[\"response_body\","), "response_body not found")
+
+    queue = make([]string, 0)
+    logger = NewHttpLoggerQueueRules(queue, "!response_body! remove_unless_found !blahblahblah!")
+    httpMessage.sendNetHttpRequestResponseMessage(logger, mockResponse, 0, 0)
+    assert.Equal(t, 1, len(queue), "queue length is not 1")
+    assert.Equal(t, true, strings.Contains(queue[0], "[\"request_body\","), "request_body not found")
+    assert.Equal(t, false, strings.Contains(queue[0], "[\"response_body\","), "response_body not removed")
+
+    queue = make([]string, 0)
+    logger = NewHttpLoggerQueueRules(queue, "!response_body|request_body! remove_unless_found !World!")
+    httpMessage.sendNetHttpRequestResponseMessage(logger, mockResponse, 0, 0)
+    assert.Equal(t, 1, len(queue), "queue length is not 1")
+    assert.Equal(t, false, strings.Contains(queue[0], "[\"request_body\","), "request_body not removed")
+    assert.Equal(t, true, strings.Contains(queue[0], "[\"response_body\","), "response_body not found")
+
+
+}
+
 // test uses replace rules
 
 // test uses replace rules with complex expressions
