13df53047cb1a808f43441e3c7ba1ea6726df2dc
devinmagnuson1@gmail.com <devinmagnuson1@gmail.com>
2021-03-03 15:56:56 -0800

    HttpLogger Constructors done along with HttpLogger struct definition

--- src/logger/HttpLogger.go
+++ src/logger/HttpLogger.go
@@ -5,23 +5,128 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
+//var AGENT string = "HttpLogger.go"
+
+func NewHttpLogger(ex_url string, 
+				   ex_enabled bool, ex_queue []string, 
+				   ex_rules string) *HttpLogger { 
+
+exHttpLogger := &HttpLogger{
+	enabled: 			ex_enabled,
+	queue: 				ex_queue,
+	skip_compression:	false,
+	skip_submission:	false,
+	rules:				ex_rules,
+	url:				ex_url
+	}
+	return exHttpLogger
+}
+
+// Some of them say initializing a logger using default url and rules but don't
+// actually pass those as parameters
+
+ /**
+ * Initialize logger using default url and default rules.
+ */
+func NewHttpLogger() *HttpLogger {
+	return
+}
+
+/**
+* Initialize enabled/disabled logger using default url and default rules.
+*/
+func NewHttpLoggerEnabled(ex_enabled bool) *HttpLogger {
+	return
+}
+
+/**
+* Initialize logger using specified url and default rules.
+*/
+func NewHttpLoggerUrl(ex_url string) *HttpLogger {
+	return
+}
+
+/**
+* Initialize logger using specified url and specified rules.
+*/
+func NewHttpLoggerUrlRules(ex_url string, ex_rules string) *HttpLogger {
+	return
+}
+
+/**
+* Initialize enabled/disabled logger using specified url and default rules.
+*/
+
+func NewHttpLoggerUrlEnabled(ex_url string, ex_enabled bool) *HttpLogger {
+	return
+}
+
+/**
+* Initialize enabled/disabled logger using specified url and specified rules.
+*/
+
+func NewHttpLoggerUrlEnabledRules(ex_url string, ex_enabled bool, ex_rules string) *HttpLogger{
+	return
+}
+
+/**
+* Initialize enabled logger using queue and default rules.
+*/
+func NewHttpLoggerQueue(ex_queue []string) *HttpLogger {
+	return
+} 
+
+/**
+* Initialize enabled logger using queue and specified rules.
+*/
+func NewHttpLoggerQueueRules(ex_queue []string, ex_rules string) *HttpLogger {
+	return
+}
+
+/**
+* Initialize enabled/disabled logger using queue and default rules.
+*/
+func NewHttpLoggerQueueEnabled(ex_queue []string, ex_enabled bool) *HttpLogger {
+	return
+}
+
+/**
+* Initialize enabled/disabled logger using queue and specified rules.
+*/
+func NewHttpLoggerQueueEnabledRules(ex_queue []string, ex_enabled bool, ex_rules string) *HttpLogger {
+	return
+}
+
+//base HttpLogger definition
 type HttpLogger struct{
-	agent string
 	enabled bool
 	queue []string
-	skipCompression bool
-	skipSubmission bool
+	skip_compression bool
+	skip_submission bool
 	rules string
 	url string
 
-
 }
 
-func initialize(var rules string){
+/**
+* Initialize a new logger.
+*/
 
+//slightly confused on the implementation here
+func initialize(ex_rules string){
+
+	// specific rules we need to account for
 	rules = new HttpRules(rules)
+
+	// IS allow_http_url allowed in our golang implementation
+	if((ex_url != nil) && (ex_url.HasPrefix("http:") && ! ex_rules.allow_http_url)) {
+		// WHERE DID enableable come from
+		ex_enableable = false; // NOT DEFINED in struct yet!!!!!!!!!!!
+		ex_enabled = false;
+	}
 
 	
 }
@@ -30,8 +135,9 @@ func HttpRules getRules(){
 	return rules
 }
 
+
 func submitIfPassing(var details []string){
-	details = rules.apply(details)
+	details = rules.apply(details) // MMMMMMMMMMMMMMMMMMMM try again
 
 	if details == nil {
 		return
