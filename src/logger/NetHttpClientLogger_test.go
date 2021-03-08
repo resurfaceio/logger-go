0c22c0785a0c030ef878d6164ba36741b02135f3
ChandlerGarthwaite <chandler.garthwaite@gmail.com>
2021-03-08 12:39:28 -0700

    Addition of new options struct and new queue, still need postform

--- src/logger/NetHttpClientLogger_test.go
+++ src/logger/NetHttpClientLogger_test.go
@@ -13,10 +13,15 @@ import (
 func TestLogsGet(t *testing.T) {
 
 	queue := make([]string, 0)
-	netLogger := NewHttpLoggerQueue(queue)
+	options := Options{
+		queue := queue,
+	}
+	netLogger := NewNetHttpClientLogger(options)
 	helper := GetTestHelper()
-	//Don't think we will need this
-	//assert.True(t, parsable(resp))
+
+	resp, err = netLogger.GET(helper.demoURL)
+
+	assert.True(t, helper.parsable(resp))
 	assert.Equal(t, true, strings.Contains(queue[0], "[\"request_method\",\"GET\"]"))
 	assert.Equal(t, true, strings.Contains(queue[0], "[\"request_url\",\""+helper.demoURL+"\"]"))
 	assert.Equal(t, true, strings.Contains(queue[0], "[\"response_body\",\""+helper.mockHTML+"\"]"))
@@ -31,28 +36,49 @@ func TestLogsGet(t *testing.T) {
 }
 
 func TestLogsPost(t *testing.T) {
-	netLogger := NewNetHttpClientLogger(nil)
+
+	queue := make([]string, 0)
+	options := Options{
+		queue := queue,
+	}
+	netLogger := NewNetHttpClientLogger(options)
 	helper := GetTestHelper()
+
 	resp, err := netLogger.Post(helper.demoURL, "text/html" /*Need IO reader format for body*/)
-	fmt.Println(resp)
-	fmt.Println(err)
-	//Don't think we will need this
-	//assert.True(t, parsable(resp))
-	assert.Contains(t, resp, "[\"request_header:content-type\",\"Application/JSON\"]")
-	assert.Contains(t, resp, "[\"request_method\",\"POST\"]")
-	assert.Contains(t, resp, "[\"request_param:message\",\""+helper.mockJSONescaped+"\"]")
-	assert.Contains(t, resp, "[\"request_url\",\""+helper.mockURL+"?"+helper.mockQueryString+"\"]")
-	assert.Contains(t, resp, "[\"response_body\",\""+helper.mockJSONescaped+"\"]")
-	assert.Contains(t, resp, "[\"response_code\",\"200\"]")
-	assert.Contains(t, resp, "[\"response_header:content-type\",\"application/json; charset=utf-8\"]")
+
+	assert.True(t, parsable(resp))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"request_header:content-type\",\"Application/JSON\"]"))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"request_method\",\"POST\"]"))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"request_param:message\",\""+helper.mockJSONescaped+"\"]"))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"request_url\",\""+helper.mockURL+"?"+helper.mockQueryString+"\"]"))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"response_body\",\""+helper.mockJSONescaped+"\"]"))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"response_code\",\"200\"]"))
+	assert.Equal(t,true,strings.Contains(queue[0],"[\"response_header:content-type\",\"application/json; charset=utf-8\"]"))
 }
 
+//Similar to Get Just doesn't return a body.
 func TestLogsHead(t *testing.T) {
-	netLogger := NewNetHttpClientLogger(nil)
+	queue := make([]string, 0)
+	options := Options{
+		queue := queue,
+	}
+	netLogger := NewNetHttpClientLogger(options)
 	helper := GetTestHelper()
+
 	resp, err := netLogger.Head(helper.demoURL)
-	fmt.Println(resp)
-	fmt.Println(err)
+
+	assert.True(t, helper.parsable(resp))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"request_method\",\"HEAD\"]"))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"request_url\",\""+helper.demoURL+"\"]"))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"response_code\",\"200\"]"))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"response_header:a\",\"Z\"]"))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"response_header:content-type\",\"text/html\"]"))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"now\",\""))
+	assert.Equal(t, true, strings.Contains(queue[0], "[\"interval\",\""))
+	assert.NotEqual(t, true, strings.Contains(queue[0], "request_body"))
+	assert.NotEqual(t, true, strings.Contains(queue[0], "request_header"))
+	assert.NotEqual(t, true, strings.Contains(queue[0], "request_param"))
+
 }
 
 func TestLogsPostForm(t *testing.T) {
