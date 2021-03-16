5042a0b53a1d811314b5dd044cc90e3e534c650a
KyleNeubarth <kyle@neubarth.org>
2021-03-16 12:53:54 -0600

    add tim's baselogger code

---
+++ src/logger/BaseLogger.go
@@ -0,0 +1,172 @@
+package logger
+
+import (
+	"bytes"
+	"compress/flate"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"sync/atomic"
+)
+
+type Options struct {
+	rules   string
+	agent   string
+	url     string
+	enabled bool
+	queue   []string
+}
+
+// BaseLogger constructor
+func NewBaseLogger(_agent string, _url string, _enabled bool, _queue []string) *BaseLogger {
+	usageLoggers, _ := GetUsageLoggers()
+
+	if _url == "" {
+		_url = usageLoggers.UrlByDefault()
+		if _url == "" {
+			_enabled = false
+		}
+	}
+
+	var _urlParsed *url.URL
+	var parsingError error
+	//validate url when present
+	if _url != "" {
+		_urlParsed, parsingError = url.Parse(_url)
+		if parsingError != nil {
+			_url = ""
+			_urlParsed = nil
+			_enabled = false
+		}
+	}
+
+	_enableable := (_url != "")
+
+	constructedBaseLogger := &BaseLogger{
+		agent:           _agent,
+		enableable:      _enableable,
+		enabled:         _enabled,
+		host:            hostLookup(),
+		queue:           _queue,
+		skipCompression: false,
+		skipSubmission:  false,
+		url:             _url,
+		urlParsed:       _urlParsed,
+		version:         versionLookup(),
+	}
+	return constructedBaseLogger
+}
+
+func (logger BaseLogger) Enable() {
+	logger.enabled = true
+}
+
+func (logger BaseLogger) Disable() {
+	logger.enabled = false
+}
+
+/**
+ * Submits JSON message to intended destination.
+ */
+func (logger BaseLogger) Submit(msg string) {
+	//woah congrats you submitted the message
+	if msg == "" || logger.SkipSubmission() || !logger.Enabled() {
+		//do nothing
+	} else if logger.queue != nil {
+		logger.queue = append(logger.queue, msg)
+		atomic.AddInt64(&logger.submitSuccesses, 1)
+	} else {
+		// not 100% sure this works (needs testing) and should add some error handling
+		submitRequest, err := http.NewRequest("POST", logger.url, bytes.NewBuffer([]byte(msg)))
+
+		// submitRequest.Method = "POST"
+		// submitRequest.URL = logger.urlParsed
+
+		submitRequest.Header.Set("Content-Type", "application/json; charset=UTF-8")
+		submitRequest.Header.Set("User-Agent", "Resurface/"+logger.version+" ("+logger.agent+")")
+
+		if !logger.skipCompression {
+			submitRequest.Header.Set("Content-Encoding", "deflated")
+
+			var b bytes.Buffer
+			w, err := flate.NewWriter(&b, 0)
+			if err != nil {
+				fmt.Errorf("Error applying deflate compression to message: ", err.Error())
+			}
+			w.Write([]byte(msg))
+			w.Close()
+
+			submitRequest.Write(w)
+		}
+
+		submitResponse, err := http.DefaultClient.Do(submitRequest)
+		if err != nil {
+			atomic.AddInt64(&logger.submitFailures, 1)
+		}
+
+		if submitResponse.StatusCode == 204 {
+			atomic.AddInt64(&logger.submitSuccesses, 1)
+		} else {
+			atomic.AddInt64(&logger.submitFailures, 1)
+		}
+	}
+}
+
+/**
+ * Returns host identifier.
+ * These are utility functions that can be static if this wasn't Go
+ */
+func hostLookup() string {
+	dyno, dynoIsPresent := os.LookupEnv("DYNO")
+	if dynoIsPresent && dyno != "" {
+		return dyno
+	}
+
+	hostName, err := os.Hostname()
+	if err != nil {
+		return "unknown"
+	}
+
+	return hostName
+}
+
+func versionLookup() string { return "1.0.0" }
+
+func (logger *BaseLogger) Agent() string    { return logger.agent }
+func (logger *BaseLogger) Enableable() bool { return logger.enableable }
+func (logger *BaseLogger) Enabled() bool {
+	usageLoggers, _ := GetUsageLoggers()
+	return logger.enabled && usageLoggers.IsEnabled()
+}
+func (logger *BaseLogger) Host() string           { return logger.host }
+func (logger *BaseLogger) Queue() []string        { return logger.queue }
+func (logger *BaseLogger) SkipCompression() bool  { return logger.skipCompression }
+func (logger *BaseLogger) SkipSubmission() bool   { return logger.skipSubmission }
+func (logger *BaseLogger) SubmitFailures() int64  { return logger.submitFailures }
+func (logger *BaseLogger) SubmitSuccesses() int64 { return logger.submitSuccesses }
+func (logger *BaseLogger) Url() string            { return logger.url }
+func (logger *BaseLogger) UrlParsed() *url.URL    { return logger.urlParsed }
+func (logger *BaseLogger) Version() string        { return logger.version }
+
+func (logger BaseLogger) SetSkipCompression(_skipCompression bool) {
+	logger.skipCompression = _skipCompression
+}
+func (logger BaseLogger) SetSkipSubmission(_skipSubmission bool) {
+	logger.skipSubmission = _skipSubmission
+}
+
+type BaseLogger struct {
+	agent           string
+	enableable      bool
+	enabled         bool
+	host            string
+	queue           []string
+	skipCompression bool
+	skipSubmission  bool
+	submitFailures  int64
+	submitSuccesses int64
+	url             string
+	urlParsed       *url.URL
+	version         string
+}
