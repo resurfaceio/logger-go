b26180475c256f39bd7b5553977662f13a285ae0
Timothy Lenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-03-16 13:00:09 -0600

    WIP

--- src/logger/HttpRules.go
+++ src/logger/HttpRules.go
@@ -5,43 +5,100 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/textproto"
+	"regexp"
 	"strings"
+	"io/ioutil"
 )
 
+var httpRules *HttpRules
+
 type HttpRules struct{
-	DEBUG_RULES		string
-	STANDARD_RULES	string
-	STRICT_RULES	string
-	DEFAULT_RULES	string
+	debugRules		string
+	standardRules	string
+	strictRules		string
+	defaultRules	string
+	allowHttpUrl	[]HttpRule
+	copySessionField	[]HttpRule
+	remove			[]HttpRule
+	removeIf 		[]HttpRule
+	removeIfFound		[]HttpRule
+	removeUnless		[]HttpRule
+	removeUnlessFound 	[]HttpRule
+	replace 			[]HttpeRule
+	sample			[]HttpRule
+	skipCompression		bool
+	skipSubmission		bool
+	size 			int
+	stop			[]HttpRule
+	stopIf 			[]HttpRule
+	stopIfFound 	[]HttpRule
+	stopUnless 		[]HttpRule
+	stopUnlessFound 	[]HttpRule
+	text 			string
 }
 
-func newHttpRules(string rules) HttpRules{
+func GetHttpRules() *HttpRules {
+
+}
+
+func NewHttpRules(string rules) *HttpRules {
+	httpRules := GetHttpRules()
 	if(rules == null){
-		rules = HttpRules.getDefaultRules()
+		rules = httpRules.getDefaultRules()
 	}
 
 	//load rules from external files
-	if(rules[0:6] == "file://"){
-		string rfile = rules[0:6]
-		//try to read rules from file
+	if(strings.HasPrefix(rules, "file://")){
+		// obtain file name
+		rfile := strings.TrimPrefix("file://")
+		rfile := strings.TrimSpace(rfile)
+		
+		// read rules from file
+		buffer, err := ioutil.ReadFile(rfile)
+		if err != nil {
+			// error handling
+		}
+		rules = string(buffer)
 	}
 
 	//force default rules if necessary
-	strings.Replace(rules, "(?m)^\\s*include default\\s*$", /*Not sure exactly what its trying to replace it with*/)
-	if(len(strings.Trimspace(rules)) == 0){
+	regex := regexp.MustCompile("(?m)^\\s*include default\\s*$")
+	rules = regex.ReplaceAllString(rules, httpRules.defaultRules)
+	if(len(strings.TrimSpace(rules)) == 0){
 		rules = HttpRules.getDefaultRules
 	}
 
 	//expand rule includes
-	strings.Replace(rules, "(?m)^\\s*include debug\\s*$", )
-	strings.Replace(rules, "(?m)^\\s*include standard\\s*$", )
-	strings.Replace(rules, "(?m)^\\s*include strict\\s*$", )
+
+	//include debug rules
+	regex = regexp.MustCompile("(?m)^\\s*include debug\\s*$")
+	rules = regex.ReplaceAllString(rules, httpRules.debugRules)
+	// include standard rules
+	regex = regexp.MustCompile("(?m)^\\s*include standard\\s*$")
+	rules = regex.ReplaceAllString(rules, httpRules.standardRules)
+	// include strict rules
+	regex = regexp.MustCompile( "(?m)^\\s*include strict\\s*$")
+	rules = regex.ReplaceAllString(rules, httpRules.strictRules)
+
+	_text := rules
 
 	// parse all rules
 	// Not sure about this part
+	var prs []HttpRule
+	for _, rule := range regexp.MustCompile("\\r?\\n").Split(_text, -1) {
+		parsed := parseRule(rule)
+		if parsed != nil {
+			prs = append(prs, parsed)
+		}
+	}
+
+	_size := len(prs)
 
 	// break out rules by verb
-	// Not sure either but may need to add to the struct created at the top
+	_allowHttpUrl := len(ruleFilter(prs, "allow_http_url", ruleCompare)) > 0
+	_copySessionField
+
 }
 
 func (HttpRules) getDefaultRules() string{
@@ -68,4 +125,43 @@ func parseRule(string r) HttpRule{
 
 	//https://www.geeksforgeeks.org/matching-using-regexp-in-golang/
 
+}
+
+/*
+ ALL REGEX STRINGS NEED TO BE COVERTED TO STRINGS THAT GO CAN COMPILE
+
+ THESE vvvv DON'T WORK vvvv
+*/
+
+
+const RegexAllowHttpUrl regexp.Regexp = regexp.MustCompile("/^\s*allow_http_url\s*(#.*)?$/")
+const RegexBlankOrComment regexp.Regexp = regexp.MustCompile("/^\s*([#].*)*$/")
+const RegexCopySessionField regexp.Regexp = regexp.MustCompile("/^\s*copy_session_field\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexRemove regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*remove\s*(#.*)?$/")
+const RegexRemoveIf regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*remove_if\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexRemoveIfFound regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*remove_if_found\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexRemoveUnless regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*remove_unless\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexRemoveUnlessFound regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*remove_unless_found\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexReplace regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*replace[\s]+([~!%|\/].+[~!%|\/]),[\s]+([~!%|\/].*[~!%|\/])\s*(#.*)?$/")
+const RegexSample regexp.Regexp = regexp.MustCompile("/^\s*sample\s+(\d+)\s*(#.*)?$/")
+const RegexSkipCompression regexp.Regexp = regexp.MustCompile("/^\s*skip_compression\s*(#.*)?$/")
+const RegexSkipSubmission regexp.Regexp = regexp.MustCompile("/^\s*skip_submission\s*(#.*)?$/")
+const RegexStop regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*stop\s*(#.*)?$/")
+const RegexStopIf regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*stop_if\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexStopIfFound regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*stop_if_found\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexStopUnless regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*stop_unless\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+const RegexStopUnlessFound regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*stop_unless_found\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
+
+func ruleCompare(ruleString string, s string) bool {
+	return rule == s
+}
+
+func ruleFilter(parsedRules []HttpRule, ruleString string, cond func(string,HttpRule) bool) []HttpRule {
+	result := []HttpRule{}
+	for _, rule := range parsedRules {
+		if cond(ruleString, rule) {
+			result = append(result, rule)
+		}
+	}
+	return result
 }
