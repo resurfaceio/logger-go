3f96d2c659887eff5b2983a32113a6acca6d5f97
Timothy Lenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-03-16 18:40:20 -0600

    WIP

--- src/logger/HttpRules.go
+++ src/logger/HttpRules.go
@@ -42,6 +42,7 @@ func GetHttpRules() *HttpRules {
 
 }
 
+// !!! this will need to return an error as well !!!
 func NewHttpRules(string rules) *HttpRules {
 	httpRules := GetHttpRules()
 	if(rules == null){
@@ -97,34 +98,111 @@ func NewHttpRules(string rules) *HttpRules {
 
 	// break out rules by verb
 	_allowHttpUrl := len(ruleFilter(prs, "allow_http_url", ruleCompare)) > 0
-	_copySessionField
+	_copySessionField := ruleFilter(prs, "copy_session_field", ruleCompare)
+	_remove := ruleFilter(prs, "remove", ruleCompare)
+	_removIf := ruleFilter(prs, "remove_if", ruleCompare)
+	_removeIfFound := ruleFilter(prs, "remove_if_found", ruleCompare)
+	_removeUnless := ruleFilter(prs, "remove_unless", ruleCompare)
+	_removeUnlessFound := ruleFilter(prs, "remove_unless_found", ruleCompare)
+	_replace := ruleFilter(prs, "replace", ruleCompare)
+	_sample := ruleFilter(prs, "sample", ruleCompare)
+	_skipCompression := ruleFilter(prs, "skip_compression", ruleCompare)
+	_skipSubmission := ruleFilter(prs, "skip_submission", ruleCompare)
+	_stop := ruleFilter(prs, "stop", ruleCompare)
+	_stopIf := ruleFilter(prs, "stop_if", ruleCompare)
+	_stopIfFound := ruleFilter(prs, "stop_if_found", ruleCompare)
+	_stopUnless := ruleFilter(prs, "stop_unless", ruleCompare)
+	_stopUnlessFound := ruleFilter(prs, "stop_unless_found", ruleCompare)
 
+	if len(_sample) > 1 {
+		// return error "Multiple sample rules"
+	}
 }
 
-func (HttpRules) getDefaultRules() string{
-	return HttpRules.DEFAULT_RULES
+func (rules *HttpRules) getDefaultRules() string{
+	return HttpRules.defaultRules
 }
 
-func (HttpRules) setDefaultRules(string r){
-	HttpRules.DEFAULT_RULES := strings.Replace(r,"(?m)^\\s*include default\\s*$","",-1)
+func (rules *HttpRules) setDefaultRules(string r){
+	regex := regexp.MustCompile("(?m)^\\s*include default\\s*$")
+	rules.defaultRules = regex.ReplaceAllString(r, "")
 }
 
 func (HttpRules) getDebugRules() string{
-	return HttpRules.DEBUG_RULES
+	return HttpRules.debugRules
 }
 
 func (HttpRules) getStandardRules() string{
-	return HttpRules.STANDARD_RULES
+	return HttpRules.standardRules
 }
 
 func (HttpRules) getStrictRules() string{
-	return HttpRules.STRICT_RULES
+	return HttpRules.strictRules
 }
 
-func parseRule(string r) HttpRule{
+// parse rule from single line
+// !!! will return error eventually !!! 
+func parseRule(string r) HttpRule /* error */ {
+	if r == "" || RegexBlankOrComment.MatchString(r) {
+		return nil
+	}
+	if RegexAllowHttpUrl.MatchString(r) {
+		return NewHttpRule("allow_http_url", nil, nil, nil)
+	}
+	if RegexCopySessionField.MatchString(r) {
+		return NewHttpRule("copy_session_field", nil, nil, nil)
+	}
+	if RegexRemove.MatchString(r) {
+		return NewHttpRule("remove", nil, nil, nil)
+	}
 
-	//https://www.geeksforgeeks.org/matching-using-regexp-in-golang/
 
+}
+
+// Parses regex for finding.
+// !!! will return error eventually !!!
+func parseRegex(r string, regex string) *regexp.Regexp /* error */ {
+	s := parseString(r, regex)
+	if "*" == s || "+" == s || "?" == s {
+		// return error '"Invalid regex (%s) in rule: %s", regex, r'
+	}
+	if strings.HasPrefix(s, "^") {
+		s = "^" + s
+	}
+	if strings.HasSuffix(s, "$") {
+		s = s + "$"
+	}
+
+	regexp, err := regexp.Compile(s)
+	if err != nil {
+		// return error '"Invalid regex (%s) in rule: %s", regex, r'
+	}
+}
+
+// Parses regex for finding.
+// !!! will return error eventually !!!
+func parseRegexFind(r string, regex string) *regexp.Regexp /* error */ {
+	regexp, err := regexp.Compile(parseString(r, regex))
+	if err != nil {
+		// return error '"Invalid regex (%s) in rule: %s", regex, r'
+	}
+}
+
+// Parses delimited string expression
+// !!! will return an error eventually !!!
+func parseString(r string, expr string) string /* error */ {
+	separators := []string{"~", "!", "%", "|", "/"}
+	for _, sep := range separators {
+		regex := regexp.MustCompile(fmt.Sprintf("^[%s](.*)[%s]$", sep, sep))
+		matches := regex.FindAllString(expr)
+		if len(matches) > 0 {
+			regex = regexp.MustCompile(fmt.Sprintf("^[%s].*|.*[^\\\\][%s].*", sep, sep))
+			if regex.MatchString(matches[0]) {
+				// return error '"Unescaped separator (%s) in rule: %s", sep, r'
+			}
+			return strings.Replace(matches[0], "\\" + sep, sep, -1)
+		}
+	}
 }
 
 /*
@@ -153,7 +231,7 @@ const RegexStopUnless regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\
 const RegexStopUnlessFound regexp.Regexp = regexp.MustCompile("/^\s*([~!%|\/].+[~!%|\/])\s*stop_unless_found\s+([~!%|\/].+[~!%|\/])\s*(#.*)?$/")
 
 func ruleCompare(ruleString string, s string) bool {
-	return rule == s
+	return rule.verb == s
 }
 
 func ruleFilter(parsedRules []HttpRule, ruleString string, cond func(string,HttpRule) bool) []HttpRule {
