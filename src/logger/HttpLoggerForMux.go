7e37dfe4bf612db401e95cf5a5172533dcd91fd7
Kolin Newby <kolin.newby@gmail.com>
2021-06-15 13:09:10 -0600

    Update HttpLoggerForMux.go

--- src/logger/HttpLoggerForMux.go
+++ src/logger/HttpLoggerForMux.go
@@ -11,12 +11,13 @@ type HttpLoggerForMux struct {
 	router mux.Router
 }
 
-func NewHttpLoggerForMux(r mux.Router, options Options) {
+func NewHttpLoggerForMux(r mux.Router) (*HttpLoggerForMux, error) {
 
+	options := Options{}
 	lgr, err := NewHttpLogger(options)
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	logger := HttpLoggerForMux{
@@ -24,4 +25,20 @@ func NewHttpLoggerForMux(r mux.Router, options Options) {
 		router: r,
 	}
 
+}
+
+func NewHttpLoggerForMuxOptions(r mux.Router, options Options) (*HttpLoggerForMux, error) {
+
+	lgr, err := NewHttpLogger(options)
+
+	if err != nil {
+		return nil, err
+	}
+
+	logger := HttpLoggerForMux{
+		logger: *lgr,
+		router: r,
+	}
+
+	log.Println("Testing")
 }
